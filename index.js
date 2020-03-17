cc90e9cdd430205cf77d9ff1e8560d3e9c8d3ff1
Kaio <kaiopeyron@gmail.com>
2020-03-17 16:04:57 +0100

    finish app

--- index.js
+++ index.js
@@ -1,43 +1,61 @@
 const vm = new Vue({
     el: '#app',
     data: {
-        hautGauche: false,
-        hautDroite: false,
-        basGauche: false,
-        basDroite: false,
-        sequence: [],
-        squareMapping: ['hautGauche', 'hautDroite', 'basGauche', 'basDroite']
+      sequence: [],
+      tmp: [],
+      hautGauche: false,
+      hautDroite: false,
+      basGauche: false,
+      basDroite: false,
+      squareMapping: ["hautGauche", "hautDroite", "basGauche", "basDroite"]
     },
-
     methods: {
-        addNewElemToSequence() {
-            this.sequence.push(this.squareMapping[Math.floor(Math.random() * 4)])
-        },
-        allGray() {
-            this.hautGauche = false;
-            this.hautDroite = false;
-            this.basGauche = false;
-            this.basDroite = false;
-        },
-        newGame() {
-            this.sequence = [];
-            this.addNewElemToSequence();
-            this[this.sequence[0]] = true;
-
-            setTimeout(function(){
-                vm.allGray();
-            }, 300)
-        },
-        selectSquare(instruction) {
-            console.log(instruction)
+      newGame() {
+        this.sequence = [];
+        this.nextTurn();
+      },
+      nextTurn() {
+        this.addNewElemToSequence();
+        this.allGray();
+        this.playSequence(this.tmp[0]);
+      },
+      allGray() {
+        this.hautGauche = false;
+        this.hautDroite = false,
+          this.basGauche = false,
+          this.basDroite = false
+      },
+      selectSquare(carre) {
+        if (carre === this.tmp[0]) {
+          vm[carre] = true;
+          setTimeout(function() {
+            vm.allGray();
+            vm.tmp.shift();
+            if (!vm.tmp[0]) {
+              vm.nextTurn();
+            }
+          }, 400)
+        } else {
+          alert('Perdu!');
         }
-    },
-
-    computed: {
-
-    },
-    
-    watch: {
-
+      },
+      addNewElemToSequence() {
+        this.sequence.push(this.squareMapping[Math.floor(Math.random() * 4)]);
+        this.tmp = this.sequence.slice();
+      },
+      playSequence(carre) {
+        setTimeout(function () {
+          vm[carre] = true;
+          setTimeout(function () {
+            vm.allGray();
+            vm.tmp.shift();
+            if (vm.tmp[0]) {
+              vm.playSequence(vm.tmp[0]);
+            } else {
+              vm.tmp = vm.sequence.slice();
+            }
+          }, 400);
+        }, 400);
+      }
     }
-})
+  });
