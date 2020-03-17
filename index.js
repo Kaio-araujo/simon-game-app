ba1a3b3adbbc2c75c43c7994cce57730952b7c98
Kaio <kaiopeyron@gmail.com>
2020-03-17 15:01:05 +0100

    random done

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const vm = new Vue({
     el: '#app',
     data: {
-        
+        hautGauche: false,
+        hautDroite: false,
+        basGauche: false,
+        basDroite: false,
+        sequence: [],
+        squareMapping: ['hautGauche', 'hautDroite', 'basGauche', 'basDroite']
     },
 
     methods: {
-
+        addNewElemToSequence() {
+            this.sequence.push(this.squareMapping[Math.floor(Math.random() * 4)])
+        },
+        newGame() {
+            this.addNewElemToSequence();
+        }
     },
 
     computed: {
@@ -13,6 +23,6 @@ const vm = new Vue({
     },
     
     watch: {
-        
+
     }
 })
